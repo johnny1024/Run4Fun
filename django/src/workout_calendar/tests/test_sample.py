569b05f5942c0bda1e081e76e816c5b2a829d9ca
Krzysztof Wencel <kwencel@users.noreply.github.com>
2017-04-22 21:39:20 +0200

    [Django] Restructured the project.

--- django/src/workout_calendar/tests/test_sample.py
+++ django/src/workout_calendar/tests/test_sample.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 
+
 class TestingClass(TestCase):
     def test_passing(self):
         self.assertEqual(1, 1)
-
-    def test_notPassing(self):
-        self.assertEqual(1, 0)
