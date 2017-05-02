dc8634be36035b41adb4bd10a1ba5b3d5740e3d1
mpralat <pralatmarta@gmail.com>
2017-05-02 11:57:46 +0200

    Change url mapping in workout_calendar so the main url redirects to current month

--- django/src/workout_calendar/urls.py
+++ django/src/workout_calendar/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-
+import datetime
 from . import views
 
 urlpatterns = [
     # maps an empty string to  function views.index name is the name that will be used to identify the view
     # the more detailed one should be the first argument!
     url(r'^([0-9]{4})/([0-9]{2})/$', views.calendar),
-    url(r'', views.calendar, {'year': '2017', 'month': '04'}, name='index'),
+    url(r'', views.calendar, {'year': str(datetime.datetime.now().year), 'month': str(datetime.datetime.now().month)}, name='index'),
 ]
