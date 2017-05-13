e815a705f0052d4cd6f04da25b4b73fc57f08b3c
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2017-05-13 22:15:03 +0200

    Profile works. CSS added.

--- django/src/accounts/views.py
+++ django/src/accounts/views.py
@@ -24,38 +24,27 @@ def signup(request):
     return render(request, 'signup.html', {'form': form})
 
 
-# @login_required
-# @transaction.atomic
-# def profile(request):
-#     context = {'page': request.resolver_match.url_name,
-#                'user': request.user}
-#     return render_to_response('profile.html', context)
-
-
 @login_required
 @transaction.atomic
 def profile(request):
     if request.method == 'POST':
         user_form = request.user
-        # user_form = UserCreationForm(request.POST, instance=request.user)
         profile_form = ProfileForm(request.POST, instance=request.user.profile)
-        if profile_form.is_valid(): #and user_form.is_valid():
+        if profile_form.is_valid():
             print("forms are ""valid")
             user_form.save()
             profile_form.save()
             print("profile saved?")
-            # messages.success(request, _('Your profile was successfully updated!'))
             return redirect('profile')
         else:
             print("forms are not valid")
-            # messages.error(request, _('Please correct the error below.'))
     else:
         user_form = request.user
-        # user_form = UserCreationForm(instance=request.user)
         profile_form = ProfileForm(instance=request.user.profile)
     return render(request, 'profile.html', {
         'user': user_form,
-        'profile_form': profile_form
+        'profile_form': profile_form,
+        'page': request.resolver_match.url_name
     })
 
 
