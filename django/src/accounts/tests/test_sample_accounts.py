5d07754d76cfdcaf728ab07189147d482dfa1570
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2017-05-17 13:07:41 +0200

    Adding tests. Not working.

--- django/src/accounts/tests/test_sample_accounts.py
+++ django/src/accounts/tests/test_sample_accounts.py
@@ -1,9 +1,12 @@
 from unittest.mock import Mock
 
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.test import TestCase, RequestFactory
+from django.test import TestCase, RequestFactory, Client
 
 from accounts import views
+
+from accounts.form import ProfileForm
 
 
 class TestDisplayProfile(TestCase):
@@ -26,5 +29,28 @@ class TestUpdateProfile(TestCase):
         post_request.resolver_match = resolver_match_mock
         response = views.profile(post_request)
         self.assertEqual(response.status_code, 200)
+
+    def test_no_data_entered_trying_to_go_to_dashboard(self):
+        rf = RequestFactory()
+        get_request = rf.get(path='/dashboard/')
+        user = User.objects.create_user(username='testUser', password='testingPassword')
+        # user.profile.age = 12
+        # user.profile.weight = 40
+        get_request.user = user
+
+        resolver_match_mock = Mock(url_name='/dashboard/')
+        get_request.resolver_match = resolver_match_mock
+
+        response = views.profile(get_request)
+
+        #c = Client()
+        #response = c.get('/dashboard/')
+        #print("Location = " + response['Location'])
+        # response = self.client.get('/dashboard/')
+        self.assertEquals(response.status_code, 302)
+
+    def test_no_data_entered_trying_to_go_to_calendar(self):
+        response = self.client.get('/calendar/')
+        self.assertEquals(response.status_code, 302)
 
 
