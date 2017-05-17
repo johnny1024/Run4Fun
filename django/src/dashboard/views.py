282d1c6fbe002a2d7fa98b19f0ddfc383abc9a55
johnny1024 <jan1024sze@gmail.com>
2017-05-17 19:30:18 +0200

    finished logic, no tests

--- django/src/dashboard/views.py
+++ django/src/dashboard/views.py
@@ -1,10 +1,44 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from workout_calendar.models import Workout
+from django.utils import timezone
+from datetime import timedelta
 
 
 # Create your views here.
 @login_required
 def index(request):
     context = {'page': request.resolver_match.url_name,
-               'user': request.user}
+               'user': request.user,
+               'days': days_since_join(request.user),
+               'distanceWhole': whole_ran_distance(request.user),
+               'distanceWeekDone': weekly_run(request.user)[0],
+               'distanceWeekPlanned': weekly_run(request.user)[1]}
     return render_to_response('dashboard.html', context)
+
+
+def days_since_join(user):
+    return (timezone.now() - user.date_joined).days
+
+
+def whole_ran_distance(user):
+    distance = 0
+    workouts = Workout.objects.filter(user=user, done=True)
+    for w in workouts:
+        distance += w.distance
+    return distance
+
+
+def weekly_run(user):
+    number_of_today = timezone.now().weekday()
+    start = timezone.now() - timedelta(days=number_of_today)
+    end = timezone.now() + timedelta(days=6-number_of_today)
+    workouts = Workout.objects.filter(user=user)
+    planned = 0
+    done = 0
+    for workout in workouts:
+        if start.date() <= workout.date <= end.date():
+            planned += workout.distance
+            if workout.done:
+                done += workout.distance
+    return done, planned
