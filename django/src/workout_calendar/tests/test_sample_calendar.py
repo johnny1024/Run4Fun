506c742f57867ceb6965b51e1e65bf7f2480bd92
mpralat <pralatmarta@gmail.com>
2017-05-17 18:22:14 +0200

    Add two new tests and documentation

--- django/src/workout_calendar/tests/test_sample_calendar.py
+++ django/src/workout_calendar/tests/test_sample_calendar.py
@@ -1,39 +1,74 @@
-from django.test import TestCase
-from django.test.client import RequestFactory
+from django.test import SimpleTestCase
+from django.test.client import RequestFactory, Client
 from django.urls import ResolverMatch
 from workout_calendar import views
 from workout_calendar import calendar_functions
-from django.contrib.auth.models import User
+from workout_calendar.models import Workout
+import mock
 
 
-class TestDisplayForm(TestCase):
+class TestDisplayForm(SimpleTestCase):
+    def setUp(self):
+        self.user = mock.Mock(username='testUser')
+
     def test_get_method_with_date(self):
         rf = RequestFactory()
         get_request = rf.get(path='/calendar/display_form', data={'date': '2017-09-08'})
-        get_request.user = User.objects.create_user(username='testUser')
-        response = views.display_form(get_request)
-        self.assertEqual(response.status_code, 200)
+        get_request.user = self.user
+        with mock.patch.object(views, 'get_all_user_workouts') as mocked_method:
+            mocked_method.return_value = []
+            response = views.display_form(get_request)
+            self.assertEqual(response.status_code, 200)
 
     def test_get_method_no_date(self):
         rf = RequestFactory()
         get_request = rf.get(path='/calendar/display_form')
-        get_request.user = User.objects.create_user(username='testUser')
-        response = views.display_form(get_request)
-        self.assertEqual(response.status_code, 404)
+        get_request.user = self.user
+        with mock.patch.object(views, 'get_all_user_workouts') as mocked_method:
+            mocked_method.return_value = []
+            response = views.display_form(get_request)
+            self.assertEqual(response.status_code, 404)
 
 
-class TestCalendar(TestCase):
+class TestCalendar(SimpleTestCase):
+    def setUp(self):
+        self.user = mock.Mock(username='testUser')
+
     def test_calendar_get_method(self):
         rf = RequestFactory()
         get_request = rf.get(path='/calendar/')
-        get_request.user = User.objects.create_user(username='testUser')
+        get_request.user = self.user
         get_request.resolver_match = ResolverMatch(func=None, args=None, kwargs=None)
         get_request.resolver_match.url_name = 'calendar'
-        response = views.calendar(get_request, '2017', '07')
-        self.assertEqual(response.status_code, 200)
+        with mock.patch.object(views, 'get_workouts_for_calendar') as calendar_mocked_method:
+            calendar_mocked_method.return_value = []
+            response = views.calendar(get_request, '2017', '07')
+            self.assertEqual(response.status_code, 200)
+
+    def test_calendar_post_delete_method(self):
+        rf = RequestFactory()
+        post_request = rf.post(path='/calendar/')
+        post_request.user = self.user
+        post_request.data = 'delete'
+        with mock.patch.object(Workout, 'delete') as delete_mock:
+            delete_mock.return_value = True
+            response = views.calendar(post_request, '2017', '08')
+            response.client = Client()
+            self.assertRedirects(response, expected_url='/calendar/', target_status_code=302)
+
+    def test_calendar_post_update_method(self):
+        rf = RequestFactory()
+        post_request = rf.post(path='/calendar/')
+        post_request.user = self.user
+        post_request.data = 'delete'
+        with mock.patch.object(Workout, 'save') as update_mock:
+            update_mock.return_value = True
+            response = views.calendar(post_request, '2017', '08')
+            response.client = Client()
+            self.assertRedirects(response, expected_url='/calendar/', target_status_code=302)
 
 
-class TestFunctions(TestCase):
+class TestFunctions(SimpleTestCase):
     def setUp(self):
         self.calendar = calendar_functions.WorkoutCalendar([])
 
