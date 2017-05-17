a951414e6ea8589521e7c3ac2ecae74f6a2e9e67
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2017-05-17 19:54:18 +0200

    Four tests added in test_sample_accounts.

--- django/src/accounts/tests/test_sample_accounts.py
+++ django/src/accounts/tests/test_sample_accounts.py
@@ -1,6 +1,9 @@
 from unittest.mock import Mock
 from django.contrib.auth.models import User
-from django.test import TestCase, RequestFactory
+from django.test import TestCase, RequestFactory, Client
+
+# from accounts import views
+from workout_calendar import views as wc_views
 
 from accounts import views
 
@@ -25,3 +28,53 @@ class TestUpdateProfile(TestCase):
         post_request.resolver_match = resolver_match_mock
         response = views.profile(post_request)
         self.assertEqual(response.status_code, 200)
+
+
+class TestRedirect(TestCase):
+    def test_no_data_entered_trying_to_go_to_calendar(self):
+        rf = RequestFactory()
+        get_request = rf.get(path='/calendar/')
+        user = User.objects.create_user(username='testUser', password='testingPassword')
+        get_request.user = user
+        resolver_match_mock = Mock(url_name='calendar/')
+        get_request.resolver_match = resolver_match_mock
+        response = wc_views.calendar(get_request)
+        response.client = Client()
+        self.assertEquals(response.status_code, 302)
+
+    def test_data_entered_trying_to_go_to_calendar(self):
+        rf = RequestFactory()
+        get_request = rf.get(path='/calendar/')
+        user = User.objects.create_user(username='testUser', password='testingPassword')
+        user.profile.age = 12
+        user.profile.weight = 40
+        get_request.user = user
+        resolver_match_mock = Mock(url_name='calendar/')
+        get_request.resolver_match = resolver_match_mock
+        response = wc_views.calendar(get_request, year='2017', month='11')
+        response.client = Client()
+        self.assertEquals(response.status_code, 200)
+
+    def test_no_data_entered_trying_to_go_to_dashboard(self):
+        rf = RequestFactory()
+        get_request = rf.get(path='/dashboard/')
+        user = User.objects.create_user(username='testUser', password='testingPassword')
+        get_request.user = user
+        resolver_match_mock = Mock(url_name='dashboard/')
+        get_request.resolver_match = resolver_match_mock
+        response = wc_views.calendar(get_request)
+        response.client = Client()
+        self.assertEquals(response.status_code, 302)
+
+    def test_data_entered_trying_to_go_to_dashboard(self):
+        rf = RequestFactory()
+        get_request = rf.get(path='/dashboard/')
+        user = User.objects.create_user(username='testUser', password='testingPassword')
+        user.profile.age = 12
+        user.profile.weight = 40
+        get_request.user = user
+        resolver_match_mock = Mock(url_name='dashboard/')
+        get_request.resolver_match = resolver_match_mock
+        response = wc_views.calendar(get_request, year='2017', month='11')
+        response.client = Client()
+        self.assertEquals(response.status_code, 200)
