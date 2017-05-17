c415770a8f3d6e4e9fba42129f2276f7cd5b4880
Krzysztof Wencel <kwencel@users.noreply.github.com>
2017-05-17 23:01:24 +0200

    [Tests] Added tests for login and signup views

--- django/src/accounts/form.py
+++ django/src/accounts/form.py
@@ -4,6 +4,9 @@ from accounts.models import Profile
 
 
 class ProfileForm(forms.ModelForm):
+    """
+    A form used in Profile page for editing and validation purposes
+    """
     class Meta:
         model = Profile
         fields = ('age', 'sex', 'weight')
