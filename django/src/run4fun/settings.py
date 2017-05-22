3b067b3943f437d1212fd25ac7c2043dd5493471
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2017-05-22 22:33:40 +0200

    Setting bootstrap for profile, sign up and login panels.

--- django/src/run4fun/settings.py
+++ django/src/run4fun/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.core.checks import messages
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -194,4 +196,8 @@ BOOTSTRAP3 = {
         'default': 'bootstrap3.renderers.FieldRenderer',
         'inline': 'bootstrap3.renderers.InlineFieldRenderer',
     },
+}
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
 }
